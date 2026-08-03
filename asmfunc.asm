2af8794e25f9b9758167c185b38e127f5724eb1e
Liana Ho <denise_liana_ho@dlsu.edu.ph>
2026-08-03 22:31:40 +0800

    working 1-pixel conversion

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,14 +1,44 @@
+; LBYARCH S25H - G05 (Ho, Pangan)
+
 ; assembly part using x86-64
+
 section .data
-msg db "Hello World",13,10,0
+denom dq 255.0
+msg db "Calling asm!",13,10,0
+
+
 section .text
 bits 64
 default rel ; to handle address relocation
-global asmhello
+global imgCvtGrayIntToDouble
 extern printf
-asmhello:
-sub rsp, 8*5 ; caller
-lea rcx, [msg]
-call printf
-add rsp, 8*5
-ret
+
+imgCvtGrayIntToDouble:
+    
+    ; rcx = input
+    ; rdx = output
+    ; r8  = width
+    ; r9  = height
+    
+    ; move input to eax, zero extended
+    movzx eax, byte [rcx]
+    
+    ; convert to double prec float
+    cvtsi2sd xmm0, eax
+    
+    ; get 255.0 from memory
+    movsd xmm1, [denom]
+    
+    ; divide input / 255.0
+    divsd xmm0, xmm1
+    
+    ; move quotient to output
+    movsd [rdx], xmm0
+    
+    ; handle caller
+    sub rsp, 8*5 
+    lea rcx, [msg]
+    call printf
+    add rsp, 8*5
+  
+    ret
--- main.c
+++ main.c
@@ -1,12 +1,27 @@
+// LBYARCH S25H - G05 (Ho, Pangan)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <stdint.h>
 
 // Template C to x86 call
-
-extern void asmhello();
+extern void imgCvtGrayIntToDouble( 
+	const uint8_t* input, // unsigned 8-bit integer
+	double* output,       // double precision float
+	int w,
+	int h
+);
 
 int main(int argc, char* argv[]) {
-	asmhello();
+
+	uint8_t input = 64;
+	double output = 0.0;
+	int w = 1, h = 1;
+
+	imgCvtGrayIntToDouble(&input, &output, w, h);
+	
+	printf("Input: %d, Output: %f\n", input, output);
+	
 	return 0;
 }
